d9bfc62d692e002a8217d8ee64881182bc34f437
Malaika Monteiro <88961104+O-Monteir@users.noreply.github.com>
2023-03-14 11:36:28 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+import Header from "./components/Header/Header";
+// const API_KEY = "AIzaSyBf-mBYkmKoyfGEBInvrtMxeZuMQBQGiho";
+const API_KEY = "AIzaSyBhtlZbMM2PL7C10-PG94aJRdwNygcXeuw";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term);
+    }, 300);
+
+    return (
+      <div>
+        <Header/>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
